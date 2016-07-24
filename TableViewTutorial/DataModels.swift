14b0c988561cb25e5a3deab367d40aff2f6155a1
Wyatt Weber <MacBookAir2011@news-macbook-air.local>
2016-07-24 10:57:48 -0700

    set up the data structure for the demographic breakdown. This looks like it will compile but is still untested for functionality or bugs during run time.

--- TableViewTutorial/DataModels.swift
+++ TableViewTutorial/DataModels.swift
@@ -27,6 +27,8 @@ protocol Query {
 
 
 
+// an "Ask" is an object containing a single image to be rated
+// (and its associated values)
 
 class Ask: Query {
     
@@ -46,7 +48,7 @@ class Ask: Query {
 }
 
 //  a "Compare" is an object holding the two images to compare
-//  and thier associated values
+//  (and its associated values)
 
 enum CompareWinner: String {
     case photo1Won
@@ -92,6 +94,7 @@ class Compare: Query {
     var rowType: String = "\(RowType.isDual)"
     
     // MARK: Also need to implement a timePosted value *********************
+    // Maybe even a computed value that returns the time remaining using the timePosted
     
     init(title1: String, photo1: UIImage, title2: String, photo2: UIImage, timePosted time: Int) {
         
@@ -106,6 +109,99 @@ class Compare: Query {
         timePosted = time
     }
 }
+
+// MARK: BREAKDOWN
+// Here we set up the necessary structure to organize and store
+// information about the breakdown of votes from various demographics
+
+protocol hasOrientation {
+    //  avgAge might not exist if there are no votes from that category yet
+    var avgAge: Double? {get set}
+    var numVotes: Int {get}
+}
+
+class AskSex: hasOrientation {
+    var avgAge: Double? = nil
+    var rating: Double = 0.0
+    var numVotes: Int = 0
+}
+
+class CompareSex: hasOrientation {
+    var avgAge: Double? = nil
+    var votesForOne: Int = 0
+    var votesForTwo: Int = 0
+    var numVotes: Int {
+        return votesForOne + votesForTwo
+    }
+}
+
+struct Breakdown {
+    let straightWomen: hasOrientation
+    let straightMen: hasOrientation
+    let gayWomen: hasOrientation
+    let gayMen: hasOrientation
+    
+    // The total number of ratings or votes that this compare or ask has received
+    var numVotes: Int {
+        return straightMen.numVotes + straightWomen.numVotes + gayWomen.numVotes + gayMen.numVotes
+    }
+    
+    var avgAge: Double {
+        // These are the components of the overall average age that each demographic makes up.
+        // For example, if only straight people have rated the ask, the gay components will be zero
+        // In this same case, if the avg straight man was 30 and the avg straight woman was 20
+        // and there were an equal number of male and female raters, the value for 
+        // straightMenAverageAgeWeighted would be 15 (because 30*.5 = 15) and the value for
+        // straightWomenAverageAgeWeighted would be 10 (because 20*.5 = 10) and
+        // when we add all 4 components together at the end (10 + 15 + 0 + 0), we get 25, which is the correct answer
+        let straightWomenAverageAgeWeighted: Double
+        let straightMenAverageAgeWeighted: Double
+        let gayWomenAverageAgeWeighted: Double
+        let gayMenAverageAgeWeighted: Double
+        
+        // these if lets are necessary to unwrap avgAge which is optional
+        if let swAA = straightWomen.avgAge {
+            // this is actually a fraction, not a percentage because it is less than one
+            // to technically be a percentage we would multiply this number by 100:
+            let percentStraightWomen = Double(straightWomen.numVotes/self.numVotes)
+            straightWomenAverageAgeWeighted = percentStraightWomen * swAA
+        } else {
+            // if the avgAge value for a demographic is nil, we store zero to the
+            // straightWomenAverageAgeWeighted component because there are no votes from that demo.
+            straightWomenAverageAgeWeighted = 0
+        }
+        if let smAA = straightMen.avgAge {
+            let percentStraightMen = Double(straightMen.numVotes/self.numVotes)
+            straightMenAverageAgeWeighted = percentStraightMen * smAA
+        } else {
+            straightMenAverageAgeWeighted = 0
+        }
+        if let gwAA = gayWomen.avgAge {
+            let percentGayWomen = Double(gayWomen.numVotes/self.numVotes)
+            gayWomenAverageAgeWeighted = percentGayWomen * gwAA
+        } else {
+            gayWomenAverageAgeWeighted = 0
+        }
+        if let gmAA = gayMen.avgAge {
+            let percentGayMen = Double(gayMen.numVotes/self.numVotes)
+            gayMenAverageAgeWeighted = percentGayMen * gmAA
+        } else {
+            gayMenAverageAgeWeighted = 0
+        }
+
+        return straightWomenAverageAgeWeighted + straightMenAverageAgeWeighted + gayWomenAverageAgeWeighted + gayMenAverageAgeWeighted
+    }
+}
+
+
+
+
+
+
+
+
+
+
 
 
 
