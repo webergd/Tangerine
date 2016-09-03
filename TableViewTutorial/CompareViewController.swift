487291ef36b0fecd059ea354ece61993c1766c87
Wyatt Weber <MacBookAir2011@news-macbook-air.local>
2016-09-03 10:22:16 -0700

    Cleaned up some excess code and comments.

--- TableViewTutorial/CompareViewController.swift
+++ TableViewTutorial/CompareViewController.swift
@@ -31,17 +31,8 @@ class CompareViewController: UIViewController {
             self.configureView()
         }
     }
-    
-    var winnerFlag: String = WinnerFlag.first.rawValue
-    
-    enum WinnerFlag: String {
-        case first
-        case second
-        case tie
-    }
 
    func configureView() -> Void {
-        //topImageView.hidden = false
         print("configuring compare view")
         // unwraps the compare that the tableView sent over:
         guard let thisCompare = self.compare else {
@@ -65,68 +56,28 @@ class CompareViewController: UIViewController {
             if let thisLabel = self.timeRemainingLabel {
                 thisLabel.text = "\(thisCompare.timePosted)"
             }
-    
-            /*
-            if let topTangerine = self.topImageView {
-                topTangerine.image = UIImage(named: "tangerineImage1")
-                print("stored an image to topTangerine")
-            }
-            if let bottomTangerine = self.bottomImageView {
-                if 1 == 1 {
-                bottomTangerine.hidden = true
+            // For some reason, I had to unwrap the tangerine images from this same ViewController in order to 
+            // modify thier attributes inside an if-then or switch-case:
+            if let topFruitFlag = winnerImageTop, bottomFruitFlag = winnerImageBottom {
+                switch thisCompare.winner {
+                    case CompareWinner.photo1Won.rawValue:
+                        topFruitFlag.hidden = false
+                        bottomFruitFlag.hidden = true
+                    case CompareWinner.photo2Won.rawValue:
+                        topFruitFlag.hidden = true
+                        bottomFruitFlag.hidden = false
+                    case CompareWinner.itsATie.rawValue:
+                        topFruitFlag.hidden = false
+                        bottomFruitFlag.hidden = false
+                    default:
+                        print("issue with compare switch statement in CompareViewController - selected default")
                 }
             }
-            */
-            // I wonder if an if statement will work instead:
-
     
-            // This part is frustrating as fuck. I can't figure out why the system keeps seeing the winnerImageTop and winnerImageBottom as nil. It doesn't see them as nil outside of configureView().
-            // Maybe there is a way to determine who the winner is inside this method, and then change the hidden property of the images outside of this method. Maybe I can set a winner flag or something that another function uses.
-            if let topFruitFlag = winnerImageTop, bottomFruitFlag = winnerImageBottom {
-            switch thisCompare.winner {
-                case CompareWinner.photo1Won.rawValue:
-                    winnerFlag = WinnerFlag.first.rawValue
-                    print("inside switch - photo 1 won")
-                    topFruitFlag.hidden = false
-                    bottomFruitFlag.hidden = true
-                case CompareWinner.photo2Won.rawValue:
-                    //winnerFlag = WinnerFlag.second.rawValue
-                    print("inside switch - photo 2 won")
-                    //if winnerImageTop == nil {
-                    //    print("winnerImageTop is nil")
-                    //}
-                    // as of now, any instruction I put in here (except for print) seems to throw a optional nil error
-                    //unHideTopImage()
-                    topFruitFlag.hidden = true
-                    bottomFruitFlag.hidden = false
-                case CompareWinner.itsATie.rawValue:
-                    //winnerFlag = WinnerFlag.tie.rawValue
-                    topFruitFlag.hidden = false
-                    bottomFruitFlag.hidden = false
-                default:
-                    print("issue with compare switch statement in CompareViewController - selected default")
-            }
-            }
-        //self.unHideWinnerImage()
-    
-    }
-
-    func unHideWinnerImage(){
-        switch winnerFlag {
-        case WinnerFlag.first.rawValue: winnerImageTop.hidden = false; winnerImageBottom.hidden = true
-        case WinnerFlag.second.rawValue: print()//winnerImageTop.hidden = true; winnerImageBottom.hidden = false
-        case WinnerFlag.tie.rawValue: winnerImageTop.hidden = false; winnerImageBottom.hidden = false
-        default: winnerImageTop.hidden = false; winnerImageBottom.hidden = false
-        }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //winnerImageTop.image = UIImage(named: "tangerineImage1")
-        if 1 == 1 {
-        winnerImageBottom.hidden = false
-        winnerImageBottom.image = UIImage(named: "tangerineImage1")
-        }
         self.configureView()
         // Do any additional setup after loading the view.
     }
